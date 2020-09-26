567b7fd134ea9e6fb5508ba1a4b75c500ab25ede
andrsGutirrz <andres.gutierrez.arcia@gmail.com>
2020-09-26 16:44:10 -0600

    pandas endpoint

--- flaskWithPandas/main.py
+++ flaskWithPandas/main.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request
+import pandas as pd
+
 
 app = Flask(__name__)
 
@@ -6,6 +8,17 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 def hello_www():
     return jsonify("Hello from GCP, Using Pandas test")
+
+
+@app.route("/api/dataframe", methods=['GET'])
+def get_simple_dataframe():
+    df = pd.DataFrame({
+        'col1': [1,2,3],
+        'col2': [4,5,6],
+        'col3': [7,8,9],
+    })
+    return df.to_json()
+
 
 if __name__ == '__main__':
       app.run(debug=True, host='0.0.0.0', port=8080)
