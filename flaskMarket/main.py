0e940c21fb5d9cd10e75db9d0683a34a6c840373
Andres Gutierrez <andres.gutierrez.arcia@gmail.com>
2020-11-12 21:53:59 -0600

    preparing GCP storage connection

--- flaskMarket/main.py
+++ flaskMarket/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
+
 import product.service as product_service
-import json
 
 app = Flask(__name__)
 
@@ -9,9 +9,12 @@ app = Flask(__name__)
 def hello_www():
     return jsonify("Hello from GCP")
 
+
 # ######################################################################################################################
 #                                               PRODUCTS
 # ######################################################################################################################
+
+
 @app.route("/api/products/products", methods=['GET'])
 def get_products():
     products = product_service.get_products()
@@ -20,11 +23,13 @@ def get_products():
         return jsonify(products_json)
     return 'Not Found', 404
 
+
 @app.route("/api/products/products", methods=['POST'])
 def create_product():
     data = request.json
     product_service.create_product(data)
     return "Created", 201
+
 
 @app.route("/api/products/products/<product_id>", methods=['GET'])
 def get_product(product_id):
@@ -34,16 +39,18 @@ def get_product(product_id):
         return jsonify(products_json)
     return 'Not Found', 404
 
+
 @app.route("/api/products/products/<product_id>", methods=['DELETE'])
 def delete_product(product_id):
     response = product_service.delete_product(product_id)
     if response:
         return 'OK', 200
     return 'Not Found', 404
+
+
 # ######################################################################################################################
 
 
-
 if __name__ == '__main__':
-      app.run(debug=True, host='0.0.0.0', port=8080)
-      # export PYTHONPATH=$(pwd)
+    app.run(debug=True, host='0.0.0.0', port=8080)
+    # export PYTHONPATH=$(pwd)
